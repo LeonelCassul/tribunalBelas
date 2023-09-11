475b21bfede97a109119abd3b583680b49340bf2
Leonel Cassul <leonelsalmos@gmail.com>
2023-09-11 11:21:02 +0100

    Css and Angular Material

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTableModule} from '@angular/material/table';
-
-
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   exports: [
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule
 
   ],
 
