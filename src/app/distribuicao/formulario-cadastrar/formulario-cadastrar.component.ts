85223087fec4b8973ab6c1d49e09f81527d9568f
Leonel Cassul <leonelsalmos@gmail.com>
2023-09-14 20:43:24 +0100

    use of pipe for icon

--- src/app/distribuicao/formulario-cadastrar/formulario-cadastrar.component.ts
+++ src/app/distribuicao/formulario-cadastrar/formulario-cadastrar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ListaProcesso } from '../model/lista-processo';
 import { ServicoListarProcessoService } from '../services/servico-listar-processo.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrosComponent } from 'src/app/shared/components/erros/erros.component';
 
 @Component({
   selector: 'app-formulario-cadastrar',
@@ -14,12 +16,26 @@ export class FormularioCadastrarComponent implements OnInit {
    displayedColumns =  ['nProcesso','arguido','seccao','juiz','data'];
 
 
-  constructor(private servicoListarProcessoService : ServicoListarProcessoService) {
+  constructor(private servicoListarProcessoService : ServicoListarProcessoService,
+              public dialog: MatDialog
+    ) {
     //this.listaProcesso = [];
-      this.listaProcesso$ = this.servicoListarProcessoService.listar().pipe(
-        catchError()
-      );
+
+      this.listaProcesso$ = this.servicoListarProcessoService.listar()
+      .pipe(catchError(error => {
+          this.darErro("Erro ao Carregar os Processos.")
+          return of([]);
+        }
+        )
+      )
   }
+
+  darErro(errorMsg:string) {
+    this.dialog.open(ErrosComponent, {
+      data: errorMsg
+    });
+  }
+
 
   ngOnInit(): void {}
 
